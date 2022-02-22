c46d81c2e0c59452e0af28141e6d22ff4dc923e9
BassamMutairi <mutairibassam@gmail.com>
2022-02-22 15:27:16 +0300

    Update: added db connection string
    Update: completed test db insert usecase
    
    Update: modified readme & lint fixes
    
    Update: combine test files

--- restAPI/controllers/controller_test.js
+++ restAPI/controllers/controller_test.js
@@ -20,3 +20,33 @@ exports.testAPI = async (req, res, next) => {
 		data: "Test API Works",
 	});
 };
+
+// DB connection testing
+const mongo_conn_native = require("../../mongo_conn_native").Connection;
+
+/**
+ * @async
+ * @route   POST /api/testAPI
+ * @returns {object} test API
+ * @author  {name}
+ * @access  public
+ * @version 1.0
+ */
+
+// eslint-disable-next-line
+exports.testDB = async (req, res, next) => {
+	// const dbInstance = mongo_conn_native.connectToMongo();
+	const client = mongo_conn_native.client;
+	try {
+		await client.db("test").collection("test").insertOne({
+			"test": "test"
+		});
+
+	} catch (e) {
+		return res.send(e);
+	}
+	res.send({
+		error: false,
+		data: "DB API Works",
+	});
+};
