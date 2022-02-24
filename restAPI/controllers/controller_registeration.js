f8262006e4ab87b29cd63a8c33c38a453441f73c
BassamMutairi <mutairibassam@gmail.com>
2022-02-24 21:29:20 +0300

    Update: inserted username and pass to db

---
+++ restAPI/controllers/controller_registeration.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const req = require("express/lib/request");
+/* eslint-disable-next-line */
+const router = express.Router();
+/* eslint-disable-next-line */
+const logger = require("../../logger").logger;
+
+// DB connection 
+const mongo_conn_native = require("../../mongo_conn_native").Connection;
+const { DATABASE, COLLECTION } = require("../../public/constant")
+
+
+/**
+ * @async
+ * @route   POST /api/testAPI
+ * @returns {object} test API
+ * @author  {name}
+ * @access  public
+ * @version 1.0
+ */
+
+// eslint-disable-next-line
+exports.registerAPI = async (req, res, next) => {
+    const client = mongo_conn_native.client
+    const username = req.body.username;
+    const pass = req.body.pass;
+
+	await client.db(DATABASE.BARCODY).collection(COLLECTION.ACCOUNTS).insertOne({
+        "username": username,
+        "pass": pass
+	});
+
+	res.status(200).send({
+		error: false,
+		result: "New user is created",
+        isCreated: true
+	});
+};
